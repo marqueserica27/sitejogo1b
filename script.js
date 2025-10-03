67187d7df813587fb6099776b5fc87e3cd9c000b
Erica Marques <s.erica27@escola.pr.gov.br>
2025-10-03 13:45:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const avanca
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+  button.addEventListener('click', function(){
+    
+  })                       
+})
